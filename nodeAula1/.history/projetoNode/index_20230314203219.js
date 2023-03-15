e2241c55ca1a545add81eee0da9c94a3bf7a8307
Gustavo Alexandre Dias <diasgustavo2000@hotmail.com>
2023-03-15 01:59:23 -0300

    .

---
+++ nodeAula1/.history/projetoNode/index_20230314203219.js
@@ -0,0 +1,27 @@
+// const http = require('http')
+// const port = 3001 
+
+// const  server = http.createServer((req, res) => {
+//     res.setHeader('Content-Type', 'text/html')
+//     res.write('<h4>Teste aula1  sobre node </h4>')
+//     res.end()
+// })
+
+// server.listen(port, () => console.log(`Servidor iniciado na porta ${port}`))
+
+const express = require('express')
+const app = express() 
+const port = 5555;
+
+app.get('/', (req, res) => {
+    res.end('')
+})
+
+app.listen(port, () => console.log(`App is running in ${port} port`))
+
+
+
+
+
+
+
---
+++ nodeAula1/.history/projetoNode/index_20230314203751.js
@@ -0,0 +1,36 @@
+// const http = require('http')
+// const port = 3001 
+
+// const  server = http.createServer((req, res) => {
+//     res.setHeader('Content-Type', 'text/html')
+//     res.write('<h4>Teste aula1  sobre node </h4>')
+//     res.end()
+// })
+
+// server.listen(port, () => console.log(`Servidor iniciado na porta ${port}`))
+
+const express = require('express')
+const app = express() 
+const port = 5555;
+
+app.get('/', (req, res) => {
+    res.setHeader('Content-Type', 'text/html')
+    res.end(`<h2> Pagina na porta ${port} e rota / </h2>`)
+})
+
+app.get('/user', (req, res) => {
+    res.setHeader('Content-Type', 'text/html')
+    res.end(`<h4> Pagina na porta ${port} e rota /user </h4>`)
+})
+
+app.listen(port, () => console.log(`App is running in ${port} port`))
+
+
+
+/* 
+    res.end  -- pode acontecer de  retornar dados incompletos, ou seja, apenas os "pedacos"  
+    res.send -- n precisa ser chamada novamente, uma vez q retorna apenas o dado COMPLETO 
+*/
+
+
+
